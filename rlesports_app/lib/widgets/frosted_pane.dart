b67fbde9b1e3ea0664e045063c2a71bd6af1af35
Jon Corbin <jon.corbin8@gmail.com>
2022-04-21 03:25:17 -0400

    create frosted pane widget

---
+++ rlesports_app/lib/widgets/frosted_pane.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:rlesports_app/assets/colors.dart';
+
+class FrostedPane extends StatelessWidget {
+  final Widget child;
+  final String label;
+
+  const FrostedPane({Key? key, required this.child, required this.label})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: .9 * MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            AppColors.whiteGradient_1.withOpacity(0.5),
+            AppColors.whiteGradient_2.withOpacity(0.5),
+          ],
+        ),
+      ),
+      child: Align(
+        alignment: Alignment.center,
+        child: Column(children: <Widget>[
+          Container(
+            child: Text(label),
+            padding: const EdgeInsets.symmetric(vertical: 5),
+          ),
+          child,
+        ]),
+      ),
+    );
+  }
+}
