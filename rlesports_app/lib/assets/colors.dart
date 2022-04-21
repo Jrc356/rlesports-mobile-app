9099e43badf0613aa1f326d21c7825bb9e626212
Jon Corbin <jon.corbin8@gmail.com>
2022-04-21 03:25:57 -0400

    add asset files

---
+++ rlesports_app/lib/assets/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const white = Color(0xffF0F0F0);
+  static const outlines = Color(0xffDEDEDE);
+
+  static const whiteGradient_1 = Color(0xffF5F5F5);
+  static const whiteGradient_2 = Color(0xffB3B3B3);
+  static const whiteGradient = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [
+      whiteGradient_1,
+      whiteGradient_2,
+    ],
+  );
+
+  static const navBarActiveIconFill = Color(0xffF5F5F5);
+  static const navBarActiveIconBorder = Color(0xff666666);
+  static const navBarInactiveIconFill = Color(0xff2A2A2A);
+  static const navBarInactiveIconBorder = Color(0xffF0F0F0);
+
+  static const rlOrange = Color(0xffFFAA00);
+  static const rlBlue = Color(0xff005AFF);
+
+  static const matteGrey = Color(0xff2A2A2A);
+}
---
+++ rlesports_app/lib/assets/sizes.dart
@@ -0,0 +1,3 @@
+class AppSizes {
+  static const double outlineWidth = 2;
+}
