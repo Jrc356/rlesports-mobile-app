75cd3dcb6596c8011990acbe3b162da003b900d9
Jon Corbin <jon.corbin8@gmail.com>
2022-04-22 15:52:21 -0400

    add option to use a backbutton on the app bar

--- rlesports_app/lib/widgets/app_bar.dart
+++ rlesports_app/lib/widgets/app_bar.dart
@@ -4,15 +4,42 @@ import 'package:rlesports_app/theme/sizes.dart';
 
 class RLAppBar extends StatelessWidget implements PreferredSizeWidget {
   final searchText = TextEditingController();
+  final bool useBackButton;
 
-  RLAppBar({Key? key}) : super(key: key);
+  RLAppBar({Key? key, this.useBackButton = false}) : super(key: key);
 
   @override
   PreferredSizeWidget build(BuildContext context) {
+    IconButton leadingIcon;
+
+    // Decide which leading icon to use
+    // if specified, use the back button
+    if (useBackButton) {
+      leadingIcon = IconButton(
+        icon: const Icon(
+          Icons.arrow_back_ios_new,
+          color: AppColors.white,
+          size: 40,
+        ),
+        onPressed: () => {Navigator.pop(context)},
+      );
+    } else {
+      leadingIcon = IconButton(
+        icon: const Icon(
+          Icons.menu,
+          color: AppColors.white,
+          size: 40,
+        ),
+        onPressed: () => {
+          print("Menu tapped"),
+        },
+      );
+    }
+
     return AppBar(
       centerTitle: true,
       toolbarHeight: preferredSize.height,
-      leading: const Icon(Icons.menu, color: AppColors.white, size: 40),
+      leading: leadingIcon,
       title: Container(
         width: double.infinity,
         height: 40,
