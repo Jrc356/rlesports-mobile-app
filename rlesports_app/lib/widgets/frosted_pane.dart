6605504687a9dc2f7774194cc396d7bd9e1ccd12
Jon Corbin <jon.corbin8@gmail.com>
2022-07-30 03:59:51 -0400

    add margin and padding params

--- rlesports_app/lib/widgets/frosted_pane.dart
+++ rlesports_app/lib/widgets/frosted_pane.dart
@@ -3,17 +3,37 @@ import 'package:rlesports_app/theme/colors.dart';
 
 class FrostedPane extends StatelessWidget {
   final Widget child;
+  final double topMargin, bottomMargin, leftMargin, rightMargin;
+  final double topPadding, bottomPadding, leftPadding, rightPadding;
+
   const FrostedPane({
     Key? key,
     required this.child,
+    this.topMargin = 5,
+    this.bottomMargin = 5,
+    this.leftMargin = 10,
+    this.rightMargin = 10,
+    this.topPadding = 10,
+    this.bottomPadding = 10,
+    this.rightPadding = 0,
+    this.leftPadding = 0,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: .9 * MediaQuery.of(context).size.width,
-      margin: const EdgeInsets.symmetric(vertical: 5),
-      padding: const EdgeInsets.symmetric(vertical: 10),
+      margin: EdgeInsets.only(
+        top: topMargin,
+        bottom: bottomMargin,
+        left: leftMargin,
+        right: rightMargin,
+      ),
+      padding: EdgeInsets.only(
+        top: topPadding,
+        bottom: bottomPadding,
+        left: leftPadding,
+        right: rightPadding,
+      ),
       decoration: BoxDecoration(
         border: Border.all(
           color: AppColors.navBarActiveIconBorder,
