d03681a5231bf4ff068f7b497da8dc2fd3a6accf
Jon Corbin <jon.corbin8@gmail.com>
2022-08-01 03:11:00 -0400

    add region; add details func

--- rlesports_app/lib/models/team.dart
+++ rlesports_app/lib/models/team.dart
@@ -1,9 +1,17 @@
 class Team {
   final String name;
   final String imageUrl;
+  final String? region;
 
   Team({
     required this.name,
     required this.imageUrl,
+    this.region,
   });
+
+  Map<String, String> getDetails() {
+    Map<String, String> details = {};
+    details["Region"] = region.toString().toUpperCase();
+    return details;
+  }
 }
