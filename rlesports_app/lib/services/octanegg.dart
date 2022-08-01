d4c2dac469549d2d28db6e8265eef896dfdd61ce
Jon Corbin <jon.corbin8@gmail.com>
2022-08-01 03:12:52 -0400

    add matches and teams getters; trim player data

--- rlesports_app/lib/services/octanegg.dart
+++ rlesports_app/lib/services/octanegg.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:rlesports_app/models/match.dart';
 import 'package:rlesports_app/models/player.dart';
+import 'package:rlesports_app/models/team.dart';
 import 'package:rlesports_app/services/liquipedia.dart' as liquid;
+import 'package:rlesports_app/constants.dart';
 
 const String api = "https://zsr.octane.gg";
 const JsonDecoder decoder = JsonDecoder();
@@ -19,12 +22,75 @@ Future<http.Response> request(
   return http.get(Uri.parse(url));
 }
 
-Future<List<Player>> getPlayers({int page = 1}) async {
+// getMatches gets a list of matches matching the specified criteria
+// TODO: Add more criteria params
+Future<List<Match>> getMatches({List<String>? tiers, int page = 1}) async {
+  List<Match> matches = [];
+
+  if (tiers != null) {
+    for (String tier in tiers) {
+      Map<String, String> reqParams = {
+        "perPage": "100",
+        "page": page.toString(),
+        "tier": tier,
+      };
+
+      // Get all matches for specified teirs
+      http.Response res = await request(endpoint: "matches", params: reqParams);
+      if (res.statusCode == 200) {
+        final data = jsonDecode(res.body);
+        for (var match in data["matches"]) {
+          Team orangeTeam = Team(name: "TBD", imageUrl: defaultImage);
+          Team blueTeam = Team(name: "TBD", imageUrl: defaultImage);
+
+          // Get orange team
+          if (match.containsKey["orange"]) {
+            var odat = match["orange"]["team"]["team"];
+            orangeTeam = Team(
+              name: odat["name"],
+              imageUrl: odat["image"],
+              region: odat["region"],
+            );
+          }
+
+          // Get blue team
+          if (match.containsKey["blue"]) {
+            var bdat = match["blue"]["team"]["team"];
+            blueTeam = Team(
+              name: bdat["name"],
+              imageUrl: bdat["image"],
+              region: bdat["region"],
+            );
+          }
+
+          // Create match
+          matches.add(
+            Match(
+              bestOf: match["format"]["length"],
+              orangeTeam: orangeTeam,
+              blueTeam: blueTeam,
+            ),
+          );
+        }
+      }
+    }
+  }
+
+  return matches;
+}
+
+// getPlayers retrieves all players matching the specified criteria
+// TODO: Add criteria params
+Future<List<Player>> getPlayers({int page = 1, int perPage = 50}) async {
   List<Player> players = [];
 
+  // TODO: Would love to sort this by tag but seems the sort parameter is broken
+  // see the discord: https://discord.com/channels/381183995423227904/833408623886598184/1003515688067014777
   http.Response res = await request(endpoint: "players", params: {
-    "perPage": "25",
+    "perPage": perPage.toString(),
     "page": page.toString(),
+    "sort": "tag:asc",
+    "relevant": "true",
   });
 
   if (res.statusCode == 200) {
@@ -36,18 +102,18 @@ Future<List<Player>> getPlayers({int page = 1}) async {
     for (var player in playerData) {
       playerNames.add(player["tag"]);
     }
+
     Map<String, String?> imagesUrls = await liquid.getImageUrls(playerNames);
 
     for (var player in playerData) {
       players.add(
         Player(
-          name: player["tag"].trim().toString(),
-          irlName: player["name"].toString(),
-          team: player["team"]?["name"].toString(),
+          name: player["tag"].toString().trim(),
+          irlName: player["name"].toString().trim(),
+          team: player["team"]?["name"].toString().trim(),
           isCoach: player.containsKey("coach") && player["coach"] == true,
-          imageUrl: imagesUrls[player["tag"]] ??
-              "https://media.sketchfab.com/models/9910f0a5d158425bbc7deb60c7a81f69/thumbnails/14ce0f826a6f4e3d9f2fb5921cd98a74/c5a09b29d7f649559972363859e7c80f.jpeg",
-          nationality: player["country"].toString(),
+          imageUrl: imagesUrls[player["tag"]] ?? defaultImage,
+          nationality: player["country"].toString().trim(),
         ),
       );
     }
@@ -56,4 +122,36 @@ Future<List<Player>> getPlayers({int page = 1}) async {
   }
 
   return players;
+}
+
+// getTeams gets teams matching the specified criteria
+// TODO: Add criteria params
+Future<List<Team>> getTeams({int page = 1, int perPage = 50}) async {
+  List<Team> teams = [];
+
+  // see the discord thread: https://discord.com/channels/381183995423227904/833408623886598184/1003515688067014777
+  http.Response res = await request(endpoint: "teams", params: {
+    "perPage": perPage.toString(),
+    "page": page.toString(),
+    "sort": "name:asc",
+    "relevant": "true",
+  });
+
+  if (res.statusCode == 200) {
+    final data = jsonDecode(res.body);
+    for (var team in data["teams"]) {
+      Team teamToAdd = Team(
+        name: team["name"],
+        imageUrl: team["image"] ?? defaultImage,
+        region: team["region"],
+      );
+      if (!teams.any((element) => element.name == teamToAdd.name)) {
+        teams.add(teamToAdd);
+      }
+    }
+  } else {
+    throw Exception("Received ${res.statusCode} code from octane: ${res.body}");
+  }
+
+  return teams;
 }
