04e675a5f2f0c6e74b6698f0d1cd2e6f94c66364
Jon Corbin <jon.corbin8@gmail.com>
2022-08-14 17:45:40 -0400

    styling and query fixes

--- rlesports_app/lib/pages/home.dart
+++ rlesports_app/lib/pages/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:rlesports_app/models/match.dart';
-import 'package:rlesports_app/theme/colors.dart';
 import 'package:rlesports_app/widgets/active_match.dart';
 import 'package:rlesports_app/widgets/background.dart';
 import 'package:rlesports_app/widgets/clickable_item.dart';
@@ -16,27 +15,24 @@ class HomePage extends StatelessWidget {
   // Get active matches
   Future<List<Match>> getActiveMatches() async {
     DateTime now = DateTime.now();
-    DateTime end = DateTime(now.year, now.month, now.day, now.hour).toUtc();
-    return octanegg.getMatches(before: end, after: end);
+    DateTime after = DateTime(now.year, now.month, now.day, now.hour).toUtc();
+    DateTime before =
+        DateTime(now.year, now.month, now.day, now.hour + 1).toUtc();
+    return octanegg.getMatches(before: before, after: after);
   }
 
   // Get scheduled matches
   Future<List<Match>> getScheduledMatches() async {
     // Scheduled matches should not include active matches
-    DateTime now = DateTime.now();
-    DateTime start =
-        DateTime(now.year, now.month, now.day, now.hour + 1).toUtc();
-    return await octanegg.getMatches(after: start);
+    return await octanegg.getMatches(after: DateTime.now().toUtc());
   }
 
   // Get past matches
   Future<List<Match>> getPastMatches() async {
     DateTime now = DateTime.now();
-    DateTime end = DateTime(now.year, now.month, now.day, now.hour - 1).toUtc();
-    DateTime start = DateTime.now().toUtc().subtract(const Duration(days: 7));
+    DateTime end = DateTime(now.year, now.month, now.day, now.hour + 1).toUtc();
     return await octanegg.getMatches(
       before: end,
-      after: start,
       sortBy: "date",
       asc: false,
     );
@@ -51,6 +47,7 @@ class HomePage extends StatelessWidget {
   }
 
   Widget buildActiveMatches(List<Match> matches) {
+    String lastEvent = "";
     return FrostedPane(
       child: Column(
         children: List.generate(
@@ -61,21 +58,44 @@ class HomePage extends StatelessWidget {
                   padding:
                       const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
                   child: Column(
-                    children: const [
-                      Text("Active Matches"),
-                      Divider(color: Colors.black),
+                    children: [
+                      const Text(
+                        "Active Matches",
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                      const Divider(color: Colors.black),
+                      if (matches.isEmpty)
+                        const ClickableItem(
+                          isClickable: false,
+                          child: Text("No Active Matches"),
+                        )
                     ],
                   ));
             }
 
-            if (matches.isEmpty) {
-              return const ClickableItem(
-                isClickable: false,
-                child: Text("No Active Matches"),
+            if (matches[index - 1].eventName != lastEvent) {
+              lastEvent = matches[index - 1].eventName.toString();
+              return Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: index - 1 == 0 ? 5 : 30),
+                    child: Text(matches[index - 1].eventName.toString()),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      left: 60,
+                      right: 60,
+                      top: 5,
+                    ),
+                    child: Divider(color: Colors.black),
+                  ),
+                  ActiveMatch(match: matches[index - 1]),
+                ],
               );
-            } else {
-              return ActiveMatch(match: matches[index - 1]);
             }
+            return ActiveMatch(match: matches[index - 1]);
           },
           growable: false,
         ),
@@ -84,8 +104,7 @@ class HomePage extends StatelessWidget {
   }
 
   Widget buildScheduledMatches(List<Match> matches) {
-    // TODO: Split by event
-    // TODO: Split by day
+    String lastEvent = "";
     return FrostedPane(
       child: Column(
         children: List.generate(
@@ -97,10 +116,34 @@ class HomePage extends StatelessWidget {
                       const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
                   child: Column(
                     children: const [
-                      Text("Scheduled Matches"),
+                      Text(
+                        "Scheduled Matches",
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
                       Divider(color: Colors.black),
                     ],
                   ));
+            }
+
+            if (matches[index - 1].eventName != lastEvent) {
+              lastEvent = matches[index - 1].eventName.toString();
+              return Column(children: [
+                Padding(
+                  padding: EdgeInsets.only(top: index - 1 == 0 ? 5 : 30),
+                  child: Text(matches[index - 1].eventName.toString()),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(
+                    left: 60,
+                    right: 60,
+                    top: 5,
+                  ),
+                  child: Divider(color: Colors.black),
+                ),
+                ScheduledMatch(match: matches[index - 1]),
+              ]);
             }
             return ScheduledMatch(match: matches[index - 1]);
           },
@@ -111,8 +154,7 @@ class HomePage extends StatelessWidget {
   }
 
   Widget buildPastMatches(List<Match> matches) {
-    // TODO: Split by event
-    // TODO: Split by day
+    String lastEvent = "";
     return FrostedPane(
       child: Column(
         children: List.generate(
@@ -124,11 +166,37 @@ class HomePage extends StatelessWidget {
                     const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
                 child: Column(
                   children: const [
-                    Text("Past Matches"),
+                    Text(
+                      "Past Matches",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
                     Divider(color: Colors.black),
                   ],
                 ),
               );
+            }
+
+            if (matches[index - 1].eventName != lastEvent) {
+              lastEvent = matches[index - 1].eventName.toString();
+              return Column(children: [
+                Padding(
+                  padding: EdgeInsets.only(top: index - 1 == 0 ? 5 : 30),
+                  child: Text(
+                    matches[index - 1].eventName.toString(),
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(
+                    left: 60,
+                    right: 60,
+                    top: 5,
+                  ),
+                  child: Divider(color: Colors.black),
+                ),
+              ]);
             }
             return PastMatch(match: matches[index - 1]);
           },
