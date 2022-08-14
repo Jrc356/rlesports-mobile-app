80b91244fc51794466ed1cedc0e80143ef7ec222
Jon Corbin <jon.corbin8@gmail.com>
2022-08-14 01:35:21 -0400

    add eventStage; move things around

--- rlesports_app/lib/models/match.dart
+++ rlesports_app/lib/models/match.dart
@@ -7,23 +7,25 @@ class Match {
       orangeTeamSetScore,
       blueTeamScore,
       blueTeamSetScore;
-  final String? timeRemaining, tier, eventName;
+  // TODO: make an event model?
+  final String? timeRemaining, tier, eventName, eventStage;
   final DateTime scheduledDateTime;
   final Team blueTeam, orangeTeam;
   final List<Game> games;
 
   Match({
     required this.bestOf,
+    required this.orangeTeam,
+    required this.blueTeam,
+    required this.scheduledDateTime,
     this.timeRemaining,
     this.orangeTeamScore,
     this.orangeTeamSetScore,
     this.blueTeamScore,
     this.blueTeamSetScore,
-    required this.orangeTeam,
-    required this.blueTeam,
-    required this.scheduledDateTime,
     this.tier,
     this.eventName,
+    this.eventStage,
     this.games = const [],
   });
 }
